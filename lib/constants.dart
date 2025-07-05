c6437ae3f6e1a9191cf3f55a1cbbd9cd5ed65802
Your Name <your_email@example.com>
2025-07-05 11:37:19 +0000

    fix: stable version with API connection

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,1 +1,25 @@
-const String backendUrl = 'https://metreyar-api.onrender.com';
+name: metreyar_flutter_web
+description: A new Flutter web project.
+publish_to: 'none'
+
+version: 1.0.0+1
+
+environment:
+  sdk: ">=3.3.0 <4.0.0"
+
+dependencies:
+  flutter:
+    sdk: flutter
+  http: ^0.13.6
+
+  # برای Flutter Web نیازی به cupertino_icons نیست، ولی اگه خواستی می‌تونی بذاری
+  cupertino_icons: ^1.0.2
+
+dev_dependencies:
+  flutter_test:
+    sdk: flutter
+
+  flutter_lints: ^3.0.0
+
+flutter:
+  uses-material-design: true
