72cee2dc50d95976f1e3dfce4e136277c6cbf54e
dilagh01 <dilagh01@users.noreply.github.com>
2025-07-05 12:52:34 +0000

    deploy: ceeb78ac97d007c17d7c7c45a59185e2df4235d6

--- main.dart.js
+++ main.dart.js
@@ -18300,7 +18300,7 @@ A.Xk.prototype={
 $1(a){var s=A.c3().b
 if(s==null)s=null
 else{s=s.canvasKitBaseUrl
-if(s==null)s=null}return(s==null?"https://www.gstatic.com/flutter-canvaskit/55eae6864b296dd9f43b2cc7577ec256e5c32a8d/":s)+a},
+if(s==null)s=null}return(s==null?"https://www.gstatic.com/flutter-canvaskit/f6344b75dcf861d8bf1f1322780b8811f982e31a/":s)+a},
 $S:94}
 A.Xy.prototype={
 $1(a){this.a.remove()
