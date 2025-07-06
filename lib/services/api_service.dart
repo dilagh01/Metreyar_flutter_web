4523797f665d6680054653765cfece4aec5695b3
Dilagh <Dilagh01@gmail.com>
2025-07-06 19:08:50 +0000

    fix: update API endpoint in api_service.dart

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class ApiService {
-  static const String baseUrl = 'https://metreyar.onrender.com';
+  static const String baseUrl = 'https://api.homkar.ir';
 
   static Future<String> fetchHello() async {
     final response = await http.get(Uri.parse('$baseUrl/'));
