30c7aeb9dd6842865eebecb5856028ba8ddae8a6
Dilagh <Dilagh01@gmail.com>
2025-07-06 19:37:39 +0000

    feat: added ApiService.ping()

--- lib/api_service.dart
+++ lib/api_service.dart
@@ -1,7 +1,7 @@
 import 'package:http/http.dart' as http;
 
 class ApiService {
-  static const String baseUrl = "https://metreyar_api.onrender.com"; // این رو به آدرس واقعی API خودت تغییر بده
+  static const String baseUrl = "https://api.homkar.ir"; // این رو به آدرس واقعی API خودت تغییر بده
 
   static Future<String> ping() async {
     final response = await http.get(Uri.parse('$baseUrl/ping'));
