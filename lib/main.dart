6f2883a843ac1f2ddc3855a6941ab9327409d38a
Dilagh <Dilagh01@gmail.com>
2025-07-06 19:17:55 +0000

    fix: updated main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,33 @@
 import 'package:flutter/material.dart';
-import 'app.dart';
+import 'services/api_service.dart';
 
 void main() {
   runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text("Metreyar Front")),
+        body: Center(
+          child: FutureBuilder<String>(
+            future: ApiService.ping(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const CircularProgressIndicator();
+              } else if (snapshot.hasError) {
+                return Text("❌ Error: ${snapshot.error}");
+              } else {
+                return Text(snapshot.data ?? "No response");
+              }
+            },
+          ),
+        ),
+      ),
+    );
+  }
 }
