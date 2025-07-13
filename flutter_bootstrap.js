b6ad0a0da2443d81ef43360f18046e8b992a6bf7
dilagh01 <dilagh01@users.noreply.github.com>
2025-07-13 05:20:24 +0000

    deploy: efb94800428b39da37a89ab30967e247b4c77a33

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"55eae6864b296dd9f43b2cc7577ec256e5c32a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2502626548"
+    serviceWorkerVersion: "168575887"
   }
 });
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -13,7 +13,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "e986ebe42ef785b27164c36a9abc7818",
 "version.json": "4ebbd80867687efc5f7a63c8198a2ea1",
 "manifest.json": "f6a19ada61a7dd91999d57dd3233282f",
-"flutter_bootstrap.js": "2835aff38d3bcea399b465dd86de54d8",
+"flutter_bootstrap.js": "a4ff9f7e991040df2df2161223496f76",
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "canvaskit/skwasm.wasm": "4051bfc27ba29bf420d17aa0c3a98bce",
 "canvaskit/canvaskit.wasm": "9251bb81ae8464c4df3b072f84aa969b",
